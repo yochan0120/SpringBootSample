19e774b6b52beb0868269bfbb325bcda4587e24e
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-01-11 23:40:19 +0900

    データベースから値を取得

---
+++ src/main/java/com/example/hello/Employee.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import lombok.Data;
+
+@Data
+public class Employee{
+	private String employeeId;
+	private String employeeName;
+	private int employeeAge;
+	
+}
+
+  
+
--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +10,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController{
 	
+	@Autowired
+	private HelloService service;
+	
 	@GetMapping("/hello")
 	public String getHello(){
-		//hello.htmlに画面遷移
-		return "hello";	
+	   //hello.htmlに画面遷移
+		return "hello/hello";
 	}
+	
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2") String id, Model model){
 		
-	@PostMapping("/hello")
-	public String postRequest(@RequestParam("text1")String str,Model model){
+		//1件検索    
+		Employee employee = service.getEmployee(id);
 		
-		//画面から受け取った文字列をModelに登録
-		model.addAttribute("sample",str);
-
-		//response.htmlに画面遷移
-		return "hello/response";	
+		//検索結果をModelに登録
+		model.addAttribute("employee",employee);
+		
+		//db.htmlに画面遷移
+		return"hello/db";
 	}
 }
+		
