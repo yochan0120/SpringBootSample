e0db85969ed7495777d79844fec4503662242907
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-03-08 09:24:10 +0900

    エラーメッセージの表示

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/user")
 @Slf4j
-public class SignupController {
+public class SignupController{
 	
 	@Autowired
 	private UserApplicationService userApplicationService;
@@ -38,8 +39,15 @@ public class SignupController {
 	
 	/**ユーザー登録処理*/
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+	public String postSignup(Model model, Locale locale,
+		@ModelAttribute SignupForm form,
+		BindingResult bindingResult) {
 		
+		//入力チェック結果
+		if(bindingResult.hasErrors()) {
+			//NG:ユーザー登録画面に戻ります
+			return getSignup(model,locale,form);
+		}
 		log.info(form.toString());
 	
 	    //ログイン画面にリダイレクト
